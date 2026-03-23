c6c186cd931e180cf3d60863dc196903dd04ae6e
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-03-23 15:30:06 -0300

    Trabalho 05 finalizado

--- 05-escolhas-ser-vivo/app.js
+++ 05-escolhas-ser-vivo/app.js
@@ -1,54 +1,206 @@
 'use strict'
 
-function removerClasse(){
-    let classe = document.getElementById('escolha2')
+const select1 = document.getElementById('select')
+const select2 = document.getElementById('escolha2')
+const select3 = document.getElementById('escolha3')
+const resultado = document.getElementById('resultado')
 
-    classe.textContent = ''
+
+select1.addEventListener('change', Escolha)
+select2.addEventListener('change', escolha2Changed)
+select3.addEventListener('change', escolha3Changed)
+
+function limparTudo(){
+    select2.innerHTML = ''
+    select3.innerHTML = ''
 }
+
+
+function limparEscolha3(){
+    select3.innerHTML = ''
+}
+
+function removerResultado(){
+    resultado.textContent = 'O resultado aparecerá aqui'
+}
+
+
 
 function Escolha(){
-    const escolha = document.getElementById('select').value
-    removerClasse()
+    let escolha = select1.value
 
-    if(escolha == 'vertebrado'){
+    limparTudo()
+
+    if(escolha === 'vertebrado'){
         classeVertebrado()
-    }else if(escolha == 'Invertebrado'){
+    } 
+    else if(escolha === 'invertebrado'){
         classeInvertebrado()
     }
-    
 }
+
 
 function classeVertebrado(){
-    let classe = document.getElementById('escolha2')
 
-    let caixaAve = document.createElement('option')
-    caixaAve.value = 'ave'
-    caixaAve.textContent = 'AVE'
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
 
-    let caixaMamifero = document.createElement('option')
-    caixaMamifero.value = 'mamifero'
-    caixaMamifero.textContent = 'MAMIFERO'
-    
-    
+    let ave = document.createElement('option')
+    ave.value = 'ave'
+    ave.textContent = 'AVE'
 
-    classe.appendChild(caixaAve)
-    classe.appendChild(caixaMamifero)
+    let mamifero = document.createElement('option')
+    mamifero.value = 'mamifero'
+    mamifero.textContent = 'MAMÍFERO'
 
+    select2.appendChild(caixaVazia)
+    select2.appendChild(ave)
+    select2.appendChild(mamifero)
 }
+
 
 function classeInvertebrado(){
-    let classe = document.getElementById('escolha2')
 
-    let caixaInseto = document.createElement('option')
-    caixaInseto.value = 'inseto'
-    caixaInseto.textContent = 'INSETO'
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
 
-    let caixaAnelideo = document.createElement('option')
-    caixaAnelideo.value = 'anelideo'
-    caixaAnelideo.textContent = 'ANELIDEO'
-    
+    let inseto = document.createElement('option')
+    inseto.value = 'inseto'
+    inseto.textContent = 'INSETO'
 
-    classe.appendChild(caixaInseto)
-    classe.appendChild(caixaAnelideo)
+    let anelideo = document.createElement('option')
+    anelideo.value = 'anelideo'
+    anelideo.textContent = 'ANELÍDEO'
+
+    select2.appendChild(caixaVazia)
+    select2.appendChild(inseto)
+    select2.appendChild(anelideo)
+}
+
+
+function escolha2Changed(){
+    let valor = select2.value
+    removerResultado()
+    limparEscolha3()
+
+    if(valor === 'ave'){
+        classeAve()
+    } 
+    else if(valor === 'mamifero'){
+        classeMamifero()
+    }else if(valor === 'inseto'){
+        classeInseto()
+    }else if(valor === 'anelideo'){
+        classeAnelideo()
+    }
+}
+
+
+// ESCOLHA 3 DAS CLASSES DOS VERTEBRADOS
+function classeAve(){
+
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
+
+    let carnivoro = document.createElement('option')
+    carnivoro.value = 'carnivoro'
+    carnivoro.textContent = 'CARNÍVORO'
+
+    let onivoro = document.createElement('option')
+    onivoro.value = 'onivoro'
+    onivoro.textContent = 'ONÍVORO'
+
+    select3.appendChild(caixaVazia)
+    select3.appendChild(carnivoro)
+    select3.appendChild(onivoro)
+
+}
+
+
+function classeMamifero(){
+
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
+
+
+    let herbivoro = document.createElement('option')
+    herbivoro.value = 'herbivoro'
+    herbivoro.textContent = 'HERBÍVORO'
+
+    let onivoro = document.createElement('option')
+    onivoro.value = 'onivoro'
+    onivoro.textContent = 'ONÍVORO'
+
+    select3.appendChild(caixaVazia)
+    select3.appendChild(herbivoro)
+    select3.appendChild(onivoro)
+}
+
+// ESCOLHA 3 DAS CLASSES DOS INVERTEBRADOS
+function classeInseto(){
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
+
+    let herbivoro = document.createElement('option')
+    herbivoro.value = 'herbivoro'
+    herbivoro.textContent = 'HERBÍVORO'
+
+    let hematofago = document.createElement('option')
+    hematofago.value = 'hematofago'
+    hematofago.textContent = 'HEMATOFAGO'
+
+    select3.appendChild(caixaVazia)
+    select3.appendChild(herbivoro)
+    select3.appendChild(hematofago)
+
+}
+
+function classeAnelideo(){
+
+    let caixaVazia = document.createElement('option')
+    caixaVazia.value = ''
+    caixaVazia.textContent = 'SELECIONE'
+
+    let onivoro = document.createElement('option')
+    onivoro.value = 'onivoro'
+    onivoro.textContent = 'ONIVORO'
+
+    let hematofago = document.createElement('option')
+    hematofago.value = 'hematofago'
+    hematofago.textContent = 'HEMATOFAGO'
+
+    select3.appendChild(caixaVazia)
+    select3.appendChild(onivoro)
+    select3.appendChild(hematofago)
     
 }
+
+function escolha3Changed(){
+    let classe = select2.value
+    let grupo = select3.value
+    removerResultado()
+
+    if(classe === 'ave' && grupo === 'carnivoro'){
+        resultado.textContent = 'AGUIA'
+    }else if(classe === 'ave' && grupo === 'onivoro'){
+       resultado.textContent = 'POMBA'
+    }else if(classe === 'mamifero' && grupo === 'onivoro'){
+        resultado.textContent = 'HOMEM'
+    }else if(classe === 'mamifero' && grupo === 'herbivoro'){
+        resultado.textContent = 'VACA'
+    }else if(classe === 'inseto' && grupo === 'hematofago'){
+        resultado.textContent = 'PULGA'
+    }else if(classe === 'inseto' && grupo === 'herbivoro'){
+        resultado.textContent = 'LAGARTA'
+    }else if(classe === 'anelideo' && grupo === 'hematofago'){
+        resultado.textContent = 'SANGUESSUGA'
+    }else if(classe === 'anelideo' && grupo === 'onivoro'){
+        resultado.textContent = 'MINHOCA'
+    }
+}
+
